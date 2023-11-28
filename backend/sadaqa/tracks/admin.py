d08b029038005f199085ceb102efe15215e1291e
osamamagar <osamamagar99@gmail.com>
2023-11-28 21:51:07 +0200

    fix some models

--- backend/sadaqa/tracks/admin.py
+++ backend/sadaqa/tracks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Track)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(CommentLike)
+admin.site.register(Playlist)
+
--- backend/sadaqa/tracks/models.py
+++ backend/sadaqa/tracks/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from accounts.models import MyUser
 
-# Create your models here.
 class Track(models.Model):
+    audio = models.FileField(upload_to='tracks/audio/',null=False)
     name = models.CharField(max_length=20,null=False)
     description=models.TextField(null=True)
     image=models.ImageField(upload_to='tracks/images',)
@@ -27,23 +27,22 @@ class Like(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     track = models.ForeignKey(Track, on_delete=models.CASCADE)
-    likes=models.BooleanField(default=False)
+    content = models.TextField() 
     created_at = models.DateTimeField(auto_now_add=True)
 
 class CommentLike(models.Model):
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    track = models.ForeignKey(Track, on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     commentLike=models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class PLaylist(models.Model):
+class Playlist(models.Model):
     name = models.CharField(max_length=50, null=False)
+    image=models.ImageField(upload_to='playlist/images')
     description = models.TextField(null=True)
     tracks = models.ManyToManyField(Track, blank=True)
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
-    image=models.ImageField(upload_to='playlist/images',)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
