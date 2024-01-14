b37f2be6f1d40286fcd4d13eef7784bed096b219
osamamagar <osamamagar99@gmail.com>
2024-01-14 02:31:20 +0200

    init after 1 month

--- backend/sadaqa/sadaqa/settings.py
+++ backend/sadaqa/sadaqa/settings.py
@@ -74,8 +74,8 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-        'rest_framework.permissions.AllowAny',
-        # 'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
